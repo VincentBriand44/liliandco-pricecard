febf782ea10b853397620f4071ba0432bd921758
VincentBriand44 <vincent-briand_student2022@wilder.school>
2023-04-23 01:02:16 +0200

    refactor: font to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,5 +6,16 @@
 		box-sizing: border-box;
 		background: #e4e4e4;
 		font-family: mediumLemonMilk;
+		user-select: none;
+	}
+
+	@font-face {
+		font-family: lightLemonMilk;
+		src: url('/src/assets/fonts/LEMONMILK-Light.otf');
+	}
+
+	@font-face {
+		font-family: mediumLemonMilk;
+		src: url('/src/assets/fonts/LEMONMILK-Medium.otf');
 	}
 </style>
