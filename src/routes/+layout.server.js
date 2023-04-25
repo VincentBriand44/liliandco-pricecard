989b7853cfff8922e6c9787fbf228cdc2f55cbdd
VincentBriand44 <vincent-briand_student2022@wilder.school>
2023-04-25 12:11:20 +0200

    fix: change position of pcv2 to 2nd

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,8 @@
 const menu = [
 	{ title: 'Price Cards', link: '/pricecards' },
+	{ title: 'Price Cards v2', link: '/pricecardsv2' },
 	{ title: 'Big Cards', link: '/bigcards' },
-	{ title: 'Cleaning Cards', link: '/cleaningcards' },
-	{ title: 'Price Cards v2', link: '/pricecardsv2' }
+	{ title: 'Cleaning Cards', link: '/cleaningcards' }
 ];
 export const prerender = true;
 
